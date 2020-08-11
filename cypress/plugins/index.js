512f6bb1c7f52053915d9a32dde60a6bd36d1b17
Sergio Gómez <sergio@uco.es>
2020-08-11 04:39:59 +0200

    Add cypress module (#2)

---
+++ cypress/plugins/index.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+  on('task', {
+    'db:clean': async () => {
+      const mongoose = require('mongoose');
+
+      await mongoose.connect(config.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      await mongoose.connection.db.dropDatabase();
+      await mongoose.disconnect();
+
+      return null;
+    },
+    sync: async () => {
+      await new Promise((r) => setTimeout(r, 50));
+
+      return null;
+    },
+  });
+};
