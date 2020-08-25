1063385d49485402a0ca7eac70b68611cdd8af96
Sergio Gómez <sergio@uco.es>
2020-08-25 15:55:29 +0200

    Add return types in functions in user module

--- src/user/infrastructure/read-model/projection/user-was-promoted.projection.ts
+++ src/user/infrastructure/read-model/projection/user-was-promoted.projection.ts
@@ -1,9 +1,9 @@
 import { Inject } from '@nestjs/common';
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { Model } from 'mongoose';
+import { UserWasPromoted } from 'src/user/domain/event';
 
 import { UserView } from '../schema/user.schema';
-import { UserWasPromoted } from 'src/user/domain/event';
 
 @EventsHandler(UserWasPromoted)
 export class UserWasPromotedProjection
@@ -12,7 +12,7 @@ export class UserWasPromotedProjection
     @Inject('USER_MODEL') private readonly userModel: Model<UserView>,
   ) {}
 
-  async handle(event: UserWasPromoted) {
+  async handle(event: UserWasPromoted): Promise<void> {
     this.userModel.updateOne({ _id: event.id }, { isAdmin: true }).exec();
   }
 }
--- src/user/infrastructure/user.provider.ts
+++ src/user/infrastructure/user.provider.ts
@@ -1,12 +1,17 @@
-import { Connection } from 'mongoose';
-import { USER_MODEL, UserSchema } from './read-model/schema/user.schema';
-import { UserEventStore } from './eventstore/user.event-store';
+import { Connection, Model } from 'mongoose';
+
 import { USERS } from '../domain/repository/users';
+import { UserEventStore } from './eventstore/user.event-store';
+import {
+  USER_MODEL,
+  UserSchema,
+  UserView,
+} from './read-model/schema/user.schema';
 
 export const UserProviders = [
   {
     provide: USER_MODEL,
-    useFactory: (connection: Connection) =>
+    useFactory: (connection: Connection): Model<UserView> =>
       connection.model('User', UserSchema),
     inject: ['DATABASE_CONNECTION'],
   },
